a21aa1bcefc21cbbcdaefc6cf6f3780f01cebbbc
user <cwhwang0203@gmail.co>
2022-03-07 19:57:54 -0500

    2nd try

--- src/lab5/Participant.java
+++ src/lab5/Participant.java
@@ -2,4 +2,17 @@ package lab5;
 
 public class Participant {
 	// TODO: implement this class
+
+    String name;
+
+    public Participant(String name) {
+        setName(name);
+    }
+    public Registration[] getRegistrations() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
